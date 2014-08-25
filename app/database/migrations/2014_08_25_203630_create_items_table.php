7fefeab9775cb16485780cddf12cb83c891a98ae
John Oeler <gooselive@gmail.com>
2014-08-25 15:41:13 -0500

    migrations

---
+++ app/database/migrations/2014_08_25_203630_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('owner_id');
+			$table->string('name');
+			$table->boolean('done');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('items');
+	}
+
+}
